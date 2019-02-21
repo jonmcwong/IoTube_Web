ede242cef0979bbe120ee9e3215b201253169f06
Jonathan Wong <jonmcwong@gmail.com>
2019-02-21 07:48:22 +0000

    website, mqtt, device integration complete

--- NEW Dashboard/importdata.js
+++ NEW Dashboard/importdata.js
@@ -1,5 +1,5 @@
 // TEST data 
-var datatest = [
+var datatest  = [ /*
     {
         "line":"circle",
         "number":"245",
@@ -35,11 +35,11 @@ var datatest = [
     {
         "line":"Bakerloo",
         "number":"25657578689",
-        "station":"Ton",
+        "station":"Uganda",
         "temp":"254",
         "co2":"5",
         "tvoc":"575465"
-    }
+    } */
 ]
 
 //INSERT DATA HERE
@@ -47,12 +47,63 @@ var data = [{"line":"Bakerloo","number":"243","station":"Paddington"},{"line":"B
 //var datacurrent = JSON.parse(data); -- use if stringyfied
 
 
+
+
+
+//Identify which line we are filtering
+var identify = document.getElementById('tag').textContent;
+
+
+//initialise mqtt client
+var client = new Paho.MQTT.Client(host, port, "/ws");
+//Set callback handlers
+client.onConnectionLost = onConnectionLost;
+client.onMessageArrived = onMessageArrived;
+client.onMessageDelivered = onMessageDelivered;
+client.onConnected = onConnected;
+var connectOptions = {
+  timeout: 3,
+  reconnect: false
+};
+var isConnectedAndSubbed = false;
+console.log("connecting to "+ host +":"+ port + "...");
+client.connect(connectOptions); //connect       
+
+//setup the page
+setupWebpage(); //mqtt connection and storage setup are both done in parrellel
+
+
+
+
+
+//setup call for webpage
+async function setupWebpage(){
+  //await setupStorage(); //add back in when storage functions defined
+  //datatest = await retrieveJSON(); // must always be in sync
+  var table = buildtable();
+  //Output table
+  document.getElementById('output').innerHTML = table;
+  setInterval(refresh, 2000);
+}
+
+//refreshing the table
+async function refresh() {
+  document.getElementById('output').innerHTML = buildtable();
+  console.log("refresh");
+  //returns array of trains currently at stations
+  var request = requestLineArrivalInfo(identify.toLowerCase()); 
+  //request data from every single one of those trains that are at stations
+  request.then((trainsJSON) => {
+    trainsJSON.forEach(function(train) {
+      //sends requests to each stationed train on its unique device topic
+      requestFromDevice(train);
+    });
+  });
+}
+
+
 // Build HTML table
 function buildtable() {
-
-    //Identify which line we are filtering
-    var identify = document.getElementById('tag').textContent;
-
     // Save Data
     var datacurrent = datatest;
 
@@ -61,24 +112,27 @@ function buildtable() {
 	table += '<table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">';
     table += '<thead><tr><th><center>STATION</center></th><th><center>TEMPERATURE</center></th><th><center>C02</center></th><th><center>VTOC</center></th><th><center>CONDITION</center></th></tr></thead>'
 	for (i = datacurrent.length - 1; i >= 0; i--) {
-        console.log(datacurrent[i].line)
+        //console.log(datacurrent[i].line)
         if(datacurrent[i].line === identify) {
             table += "<tbody><tr>";
             table += "<td>" + datacurrent[i].station + "</td>";
-            table += '<td class="check">' + datacurrent[i].temp + "</td>"
-            table += "<td>" + datacurrent[i].co2 + "</td>";
-            table += "<td>" + datacurrent[i].tvoc + "</td>";
+            table += '<td class="check">' + datacurrent[i].temperature + "</td>"
+            table += "<td>" + datacurrent[i].CO2 + "</td>";
+            table += "<td>" + datacurrent[i].VolatileComponents + "</td>";
             table += '<td class="heat">' + "</td>"
             table += "</tr></tbody>";
         }
 	}
     table += "</table>";
-
+    return table;
+    /*
     //Output table
     var output = document.getElementById('output')
     output.innerHTML = table;
+*/
 }
-buildtable();
+
+//buildtable();
 
 $(document).ready(function(){
     $('#dataTable td.check').each(function(){
